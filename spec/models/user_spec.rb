f393d747ff8cd30162604deca77f7e5d42a54ce8
Yoshi3lines <ny3line@gmail.com>
2021-08-28 10:12:33 +0900

    60ページまで読走

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
+describe User do
+  # 有効なファクトリをもつこと
+  it "has a valid factory" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
+  # 名が無ければ無効な状態であること
+  it "is invalid without a first name" do
+    user = FactoryBot.build(:user, first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+
+  # 姓が無ければ無効な状態であること
+  it "is invalid without a last name" do
+    user = FactoryBot.build(:user, last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+
+  # メールアドレスが無ければ無効な状態であること
+  it "is invalid without an email address" do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  # ユーザーのフルネームを文字列として返すこと
+  it "returns a user's full name as a string" do
+    user = FactoryBot.build(:user, first_name: "John", last_name: "Doe")
+    expect(user.name).to eq "John Doe"
+  end
+
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid without an email address" do
+    FactoryBot.create(:user, email: "aaron@example.com")
+    user = FactoryBot.build(:user, email: "aaron@example.com")
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+end
+
 RSpec.describe User, type: :model do
   # 姓、名、メール、パスワードがあれば有効な状態であること
   it "is valid with a first name, last name, email, and password" do
