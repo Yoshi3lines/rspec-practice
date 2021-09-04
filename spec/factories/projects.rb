6e5199c4429995371c0e1f3bf22d83fd5b5c2165
Yoshi3lines <ny3line@gmail.com>
2021-09-04 11:13:14 +0900

    70ページまで終了、演習問題を実施する

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,20 +4,40 @@ FactoryBot.define do
     description "Sample project for testing purposes"
     due_on 1.week.from_now
     association :owner
-    
-    # 昨日が締め切りのプロジェクト
-    factory :project_due_yesterday do
-      due_on 1.day.ago
-    end
-    
-    # 今日が締め切りのプロジェクト
-    factory :project_due_today do
-      due_on Date.current.in_time_zone
+
+    # メモ付きのプロジェクト
+    trait :with_notes do
+      after(:create) { |project| create_list(:note, 5, project: project) }
     end
 
-    # 明日が締め切りのプロジェクト
-    factory :project_due_tomorrow do
+    # 締め切りが昨日
+    trait :due_yesterday do
+      due_on 1.day.ago
+    end    
+  # before↓ after↑
+    # # 昨日が締め切りのプロジェクト
+    # factory :project_due_yesterday do
+    #   due_on 1.day.ago
+    # end
+    
+    # 締め切りが今日
+    trait :due_today do
+      due_on Date.current.in_time_zone
+    end
+  # before↓ after↑
+    # # 今日が締め切りのプロジェクト
+    # factory :project_due_today do
+    #   due_on Date.current.in_time_zone
+    # end
+
+    # 締め切りが明日
+    trait :due_tomorrow do
       due_on 1.day.from_now
     end
+  # before↓ after↑
+    # # 明日が締め切りのプロジェクト
+    # factory :project_due_tomorrow do
+    #   due_on 1.day.from_now
+    # end
   end
 end
