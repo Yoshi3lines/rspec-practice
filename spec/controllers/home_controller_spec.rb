86b19cee30cf32ef846fccdd0a74144467071684
Yoshi3lines <ny3line@gmail.com>
2021-09-07 23:10:57 +0900

    sign_inさせることによりエラーに対処

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe HomeController, type: :controller do
     # 正常にレスポンスを返すこと
     it "responds successfully" do
       get :index
-      expect(response).to be_success
+      expect(response).to have_http_status "200"
     end
   end
 end
