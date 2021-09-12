f439f477d96bacc15a9905d2922555bdeb6efac4
Yoshi3lines <ny3line@gmail.com>
2021-09-12 23:58:30 +0900

    95ページまで終了

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -37,6 +37,39 @@ RSpec.describe ProjectsController, type: :controller do
     end
   end
 
+  describe "#new" do
+    # 認可されたユーザーとして
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+
+      # 正常にレスポンスを返すこと
+      it "responds successfully" do
+        sign_in @user
+        get :new, params: { id: @project.id }
+        expect(response).to be_success
+      end
+    end
+
+    # 認可されていないユーザーとして
+    context "as an unauthorized user" do
+      before do
+        @user =FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
+
+      # ダッシュボードにリダイレクト
+      it "redirects to the dashboard" do
+        sign_in @user
+        post :new, params: { id: @project.id }
+        expect(response).to_not redirect_to root_path
+      end
+    end
+  end
+
   describe "#show" do
     # 認可されたユーザーとして
     context "as an authorized user" do
@@ -51,21 +84,21 @@ RSpec.describe ProjectsController, type: :controller do
         get :show, params: { id: @project.id }
         expect(response).to be_success
       end
+    end
 
-    # 認可されてないユーザーとして
-      context "as an unauthorized user" do
-        before do
-          @user = FactoryBot.create(:user)
-          other_user = FactoryBot.create(:user)
-          @project = FactoryBot.create(:project, owner: other_user)
-        end
+  # 認可されてないユーザーとして
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
 
-      # ダッシュボードにリダイレクトすること
-        it "redirects to the dashboard" do
-          sign_in @user
-          get :show, params: { id: @project.id }
-          expect(response).to redirect_to root_path
-        end
+    # ダッシュボードにリダイレクトすること
+      it "redirects to the dashboard" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to redirect_to root_path
       end
     end
   end
